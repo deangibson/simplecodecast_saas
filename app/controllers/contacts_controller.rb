4a9825c958b770f44a180053ac1e076013b95d2f
Dean Gibson <dean@deankgibson.com>
2015-08-21 14:39:34 +0000

    Fixed contact.rb error.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,9 +1,6 @@
 class ContactsController < ApplicationController
     def new
-<<<<<<< HEAD
         @contact = Contact.new
-=======
->>>>>>> 58a2f3c3d339f5963aeda3e47bdd0786f8900c13
     end
     
     def create
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
 class Contact < ActiveRecord::Base
     
 end
-=======
-class Contact < ActiveRecord: :Base
-    
-end
-
->>>>>>> 58a2f3c3d339f5963aeda3e47bdd0786f8900c13
