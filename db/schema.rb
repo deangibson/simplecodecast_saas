22c1319a2baa90d8c520b41e600cca6c11d52238
Dean Gibson <dean@deankgibson.com>
2015-08-22 12:02:55 +0000

    Installed devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20150821114901) do
+ActiveRecord::Schema.define(version: 20150822115825) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
@@ -20,14 +19,24 @@ ActiveRecord::Schema.define(version: 20150821114901) do
     t.text     "comments"
     t.datetime "created_at"
     t.datetime "updated_at"
-=======
-ActiveRecord::Schema.define(version: 20150817232709) do
-
-  create_table "contacts", force: true do |t|
-    t.string "name"
-    t.string "email"
-    t.text   "comments"
->>>>>>> 58a2f3c3d339f5963aeda3e47bdd0786f8900c13
   end
+
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
